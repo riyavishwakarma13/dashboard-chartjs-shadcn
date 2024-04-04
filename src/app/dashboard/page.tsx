2f8de49c1848f86651a4514ff33d6a4a27b5de76
Riya Vishwakarma <riyavishwakarma.alf@gmail.com>
2024-04-04 06:48:39 +0530

    changed => dashboard code

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,20 @@
 "use client";
-import React from "react";
+import React, { useContext } from "react";
 import LineChart from "@/charts/LineChart";
 import PieChart from "@/charts/PieChart";
 import Navbar from "@/components/Navbar";
 import UserDemographic from "@/components/UserDemographic";
+import { useRouter } from "next/navigation";
+import AuthContext from "@/lib/context";
 
 const Dashboard = () => {
+  const router = useRouter();
+  const { isLoggedIn } = useContext(AuthContext);
+
+  if (!isLoggedIn) {
+    router.push("/");
+    return null;
+  }
   return (
     <div className="max-w-7xl mx-auto">
       <Navbar />
